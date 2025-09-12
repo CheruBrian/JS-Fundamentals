8b650fdf6f10e53c573fef838bb8e6d8ea9fb6d2
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-09-12 15:19:19 +0300

    arrays strings and loops.

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,17 @@
+// Array of strings
+const languages  = {
+"C is fun",
+"Python is cool",
+"JavaScript is amazing"
+};
+
+//use of loop  through array
+let i = 0;
+let output = "";
+while( i < language.length) {
+output += language[1] + "\n";
+i++;
+}
+
+//print 
+console.log(output);
