ce95ea65060ae56fac2ebc2539b3898ff89b623a
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-09-12 15:56:57 +0300

    I love C

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const x = pareInt(process.argv{2}, 100;
+
+if (isNaN(x)) {
+console.log("Missing number of occurrences");
+}
+else {
+let i = 0;
+while (1 < x) {
+console.log('C is fun);
+i++;
+}
+}
