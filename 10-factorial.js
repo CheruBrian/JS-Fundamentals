e7a77cb3939ba468fb523292faea8c90825a9185
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-09-14 18:39:11 +0300

    Update 10-factorial.js
    Factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,12 +1,12 @@
-#node/bin
+#!/usr/bin/node
 
-const num = parseInt(process.agrv[2]);
+const num = parseInt(process.argv[2]);
 
-funtion factorial(n) {
-if (isNaN(n) || n === 0) {
-return 1;
-}
-return n * factorial(n - 1);
+function factorial(n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(num));
