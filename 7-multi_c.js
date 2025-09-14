7c8109a1b1b262dd7c11d491adc5eb6c6c744e7d
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-09-14 21:33:32 +0300

    Update 7-multi_c.js
    multi

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,13 +1,11 @@
+#!/usr/bin/node
+
 const x = parseInt(process.argv[2]);
 
 if (isNaN(x)) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
-  let i = 0;
-  let output = "";
-  while (i < x) {
-    output += "C is fun\n";
-    i++;
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
-  console.log(output.trim());
 }
